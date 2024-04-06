b7176ad532ac04feedca917525f80f8f1c579530
AliLRS <sorush.lamei@gmail.com>
2024-04-07 02:11:53 +0330

    add clear screen procedure

--- main.asm
+++ main.asm
@@ -7,13 +7,24 @@ username db ?
 seed db ?
 
 .code
+main proc
     mov ax, @data
     mov ds, ax
 
-main proc
-
-_end:
     mov ah, 4cH
     int 21H
 main endp
+
+clear_screen proc
+    mov ah, 06      ; argumant for scrolling upward
+    mov al, 00      ; number of rows that scrolled up(in this procedure, whole of screen) 
+    mov bh, 07      ; color
+    mov ch, 00
+    mov cl, 00
+    mov dh, 24
+    mov dl, 79
+    int 10h
+    ret
+clear_screen endp
+
 end main
